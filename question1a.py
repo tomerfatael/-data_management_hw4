c642d213d8328b4718cc2cfef47f124e33561e57
tomerfatael <tomerfatael@mail.tau.ac.il>
2022-06-03 23:50:54 +0300

    first commit

---
+++ question1a.py
@@ -0,0 +1,34 @@
+
+
+def load_data(filename, rank_name):
+    with open(filename) as f:
+        data = f.readlines()
+
+    ranks = {}
+    ordered = []
+    for line in data:
+        line_data = line.split(",")
+        name = line_data[0]
+        score = line_data[1]
+        ranks[name] = float(score)
+        ordered.append(name)
+
+    return dict(ranks=ranks, ordered=ordered, name=rank_name)
+
+def have_k_greater_than_T(seen, k, T):
+    cnt = 0
+    for num in seen:
+        if num >= T:
+            cnt += 1
+    return cnt >= k
+
+
+def ta_aggregation(files, k, aggregation_func):
+    dicts_lst = []
+    for file in files:
+        dicts_lst.append(load_data(file, file)) #what is rank name???
+
+    T = -1
+    seen = {}
+
+    while not(have_k_greater_than_T(seen, k, T)):
